058c74a590229dd289ad2b1681890048edbaa110
JanCieszynski <janekcieszynski1@gmail.com>
2024-10-02 12:25:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sklep dla uczniów</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header> DZISIEJSZE PROMOCJE NASZEGO SKLEPU </header>
+        <div class="columnContainer">
+            <aside class="left">
+        <h2>Taniej o 30%</h2>
+        <!-- <?php
+        // $db = new mysqli('localhost', 'root', '', 'sklep');
+        // $db = "SELECT nazwa FROM `towary` WHERE 'promocja' = 1;";
+        // $result = $query->get_result();
+        // $db -> close();
+        // while(row)
+        ?> -->
+        <ol>
+            <l1>Gumka do mazania</l1>
+            <l1>Cienkopis</l1>
+            <l1>Pisaki 60 szt.</l1>
+            <l1>Markery 4 szt.</l1>
+        </ol>
+            </aside>
+            <main>
+           <h2>Sprawdz cenę</h2>
+           <form action="index.php" method="post">
+            <select name="ItemSelect" id="">
+                <option value="">Gumka do mazania</option>
+                <option value="">Cienkopis</option>
+                <option value="">Pisak</option>
+                <option value="">Markery</optiFon>
+                <input type="submit" value="SPRAWDZ">
+            </select>
+           </form>
+            
+           <p>
+            <!-- skrypt 2 -->
+        </p>
+        </main> </aside>
+            <aside class="right">
+            <h2>Kontakt</h2>
+          <p>email: <a href="email:  bok@sklep.pl"> bok@sklep.pl</a></p>
+          <img src="promocja.png" alt="">
+            </aside>
+        </div>
+        <footer>
+            <h4>Autor strony: 00000000000</h4>
+        </footer>
+    
+</body>
+</html>
